280f8e3cfe1e9d1f594fb68c0fbfcb94a82ffb88
larissa <larissafelicianasilva@gmail.com>
2019-08-04 14:10:36 -0300

    dropbox

--- djangram/settings/production.py
+++ djangram/settings/production.py
@@ -11,7 +11,7 @@ ALLOWED_HOSTS = [
 
 # Dropbox for media files
 DEFAULT_FILE_STORAGE = 'storages.backends.dropbox.DropBoxStorage'
-DROPBOX_OAUTH2_TOKEN = config('DROPBOX_OAUTH2_TOKEN', default='')
+DROPBOX_OAUTH2_TOKEN = config('DROPBOX_OAUTH2_TOKEN', default='em1Xkx6FqgAAAAAAAAAADWVyHeQGjt6SMR6DmtRcly7Vr-yHRlxmHZ6oJdbaP5fO')
 
 # Social auth
 SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = config('SOCIAL_AUTH_GOOGLE_OAUTH2_KEY', default='')
