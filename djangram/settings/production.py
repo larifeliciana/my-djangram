c61533647bb33e2cbf4d9d107197d69b6ee1ed9d
larissa <larissafelicianasilva@gmail.com>
2019-08-04 14:34:06 -0300

    storage

--- djangram/settings/production.py
+++ djangram/settings/production.py
@@ -9,17 +9,5 @@ ALLOWED_HOSTS = [
     '.herokuapp.com',
 ]
 
-# Dropbox for media files
-#DEFAULT_FILE_STORAGE = 'storages.backends.dropbox.DropBoxStorage'
-#DROPBOX_OAUTH2_TOKEN = 'em1Xkx6FqgAAAAAAAAAADWVyHeQGjt6SMR6DmtRcly7Vr-yHRlxmHZ6oJdbaP5fO'
-
-# Social auth
-SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = config('SOCIAL_AUTH_GOOGLE_OAUTH2_KEY', default='')
-SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = config('SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET', default='')
-
-# Email
-#EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='projetosbcc2018@gmail.com')
-#	EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='@123minha123')
-
 
 django_heroku.settings(locals())
