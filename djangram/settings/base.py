d572d9870315c0eadf47cf0fc6610b5dcca8259c
larissa <larissafelicianasilva@gmail.com>
2019-08-10 14:25:55 -0300

    a

--- djangram/settings/base.py
+++ djangram/settings/base.py
@@ -12,7 +12,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
-
+import dotenv
+import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
@@ -99,6 +100,14 @@ WSGI_APPLICATION = 'djangram.wsgi.application'
 
 DATABASES = {}
 DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+
+
+#DATABASES = {
+ #   'default': {
+  #      'ENGINE': 'django.db.backends.sqlite3',
+   #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+   # }
+#}
 
 # Custom Users
 # https://docs.djangoproject.com/pt-br/2.2/topics/auth/customizing/#substituting-a-custom-user-model
