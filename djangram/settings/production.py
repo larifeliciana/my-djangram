5ba51fadba19b13288e058aecff0cc8da1457adc
larissa <larissafelicianasilva@gmail.com>
2019-08-10 13:36:54 -0300

    atualizando

--- djangram/settings/production.py
+++ djangram/settings/production.py
@@ -13,8 +13,8 @@ ALLOWED_HOSTS = [
 
 
 # Dropbox for media files
-DEFAULT_FILE_STORAGE = 'storages.backends.dropbox.DropBoxStorage'
-DROPBOX_OAUTH2_TOKEN = 'em1Xkx6FqgAAAAAAAAAAD1mfWL7HgT_gcaK55k8HU_qpO0ueC_5GKk3lNVktM3zs'
+#DEFAULT_FILE_STORAGE = 'storages.backends.dropbox.DropBoxStorage'
+#DROPBOX_OAUTH2_TOKEN = 'em1Xkx6FqgAAAAAAAAAAD1mfWL7HgT_gcaK55k8HU_qpO0ueC_5GKk3lNVktM3zs'
 
 
 django_heroku.settings(locals())
