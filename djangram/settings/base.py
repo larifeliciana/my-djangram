9ff30b8dc6362e2d12b4a8421b6e7001f7d27e2f
larissa <larissafelicianasilva@gmail.com>
2019-08-10 14:10:34 -0300

    database

--- djangram/settings/base.py
+++ djangram/settings/base.py
@@ -96,12 +96,6 @@ WSGI_APPLICATION = 'djangram.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-#DATABASES = {
- #   'default': {
- #       'ENGINE': 'django.db.backends.sqlite3',
- #       'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-  #  }
-#}
 
 DATABASES = {}
 DATABASES['default'] = dj_database_url.config(conn_max_age=600)
