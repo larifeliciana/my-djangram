24d99a2d35ad3812cfc7818d416f612f182796c4
larissa <larissafelicianasilva@gmail.com>
2019-08-10 13:27:54 -0300

    atualizando

--- djangram/settings/production.py
+++ djangram/settings/production.py
@@ -3,11 +3,18 @@ from decouple import config
 
 from djangram.settings.base import *
 
-DEBUG = True
+DEBUG = False
+
+
 
 ALLOWED_HOSTS = [
     '.herokuapp.com',
 ]
+
+
+# Dropbox for media files
+DEFAULT_FILE_STORAGE = 'storages.backends.dropbox.DropBoxStorage'
+DROPBOX_OAUTH2_TOKEN = 'em1Xkx6FqgAAAAAAAAAAD1mfWL7HgT_gcaK55k8HU_qpO0ueC_5GKk3lNVktM3zs'
 
 
 django_heroku.settings(locals())
